50d8b70ecac8807630d7aae0aa44181402c83007
John Cheng <chengjohn1997@gmail.com>
2023-04-21 22:46:05 -0400

    be able to specify a playlist to save to

--- save_discover_weekly.py
+++ save_discover_weekly.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 from functools import cached_property
 
@@ -10,8 +11,9 @@ class PlaylistNotFoundError(Exception):
 
 
 class Spotify(spotipy.Spotify):
-    def __init__(self):
-        scope = "playlist-modify-private playlist-read-private"
+    def __init__(self, playlist_name=None):
+        self.playlist_name = playlist_name
+        scope = "playlist-modify-private playlist-read-private playlist-modify-public"
         super().__init__(
             auth_manager=SpotifyOAuth(
                 scope=scope,
@@ -21,12 +23,20 @@ class Spotify(spotipy.Spotify):
     def create_discover_weekly_playlist(self):
         if self.discover_weekly_id:
             new_playlist = self.create_playlist(
-                f"{self.start_of_week} Re-Discover Weekly"
+                self.playlist_name or f"{self.start_of_week} Re-Discover Weekly"
             )
-            self.playlist_replace_items(
-                new_playlist["id"],
-                [track["track"]["id"] for track in self.discover_weekly_tracks],
-            )
+            if self.playlist_name:
+                existing_tracks = self.get_track_ids_from_playlist(new_playlist["id"])
+                new_tracks = list(
+                    self.discover_weekly_tracks.difference(existing_tracks)
+                )
+                if new_tracks:
+                    self.playlist_add_items(new_playlist["id"], new_tracks)
+            else:
+                self.playlist_replace_items(
+                    new_playlist["id"],
+                    [track_id for track_id in self.discover_weekly_tracks],
+                )
 
     def create_playlist(self, playlist_name):
         try:
@@ -41,9 +51,9 @@ class Spotify(spotipy.Spotify):
         discover_weekly_playlist = self.find_playlist("Discover Weekly")
         return discover_weekly_playlist["id"]
 
-    @cached_property
+    @property
     def discover_weekly_tracks(self):
-        return self.playlist_tracks(self.discover_weekly_id)["items"]
+        return self.get_track_ids_from_playlist(self.discover_weekly_id)
 
     def find_playlist(self, playlist_name):
         offset = 0
@@ -56,6 +66,21 @@ class Spotify(spotipy.Spotify):
                     return playlist
             offset += 50
 
+    def get_track_ids_from_playlist(self, playlist_id):
+        track_ids = set()
+        
+        offset = 0
+        while True:
+            tracks = self.playlist_tracks(playlist_id, offset=offset)
+            for track in tracks["items"]:
+                track_ids.add(track["track"]["id"])
+            if tracks["next"] is not None:
+                offset += 100
+            else:
+                break
+        
+        return track_ids
+
     @property
     def start_of_week(self):
         now = datetime.datetime.now()
@@ -64,5 +89,13 @@ class Spotify(spotipy.Spotify):
 
 
 if __name__ == "__main__":
-    spotify = Spotify()
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [PLAYLIST_NAME]",
+        description="Set name of playlist to add to or use default"
+    )
+    parser.add_argument(
+        "-pn", "--playlist_name", default=None
+    )
+    args = parser.parse_args()
+    spotify = Spotify(args.playlist_name)
     spotify.create_discover_weekly_playlist()
