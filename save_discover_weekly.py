b33a4003562cbe53b358cc4feb77bdd4809a9706
John Cheng <chengjohn1997@gmail.com>
2023-07-28 17:56:27 -0400

    add typing

--- save_discover_weekly.py
+++ save_discover_weekly.py
@@ -1,6 +1,9 @@
 import argparse
 import datetime
+from collections.abc import KeysView
+from dataclasses import dataclass
 from functools import cached_property
+from typing import Dict, Union
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -10,8 +13,14 @@ class PlaylistNotFoundError(Exception):
     pass
 
 
+@dataclass
+class Playlist:
+    name: str
+    id: str
+
+
 class Spotify(spotipy.Spotify):
-    def __init__(self, playlist_name=None):
+    def __init__(self, playlist_name: Union[str, None] = None):
         self.playlist_name = playlist_name
         scope = "playlist-modify-private playlist-read-private playlist-modify-public"
         super().__init__(
@@ -20,44 +29,46 @@ class Spotify(spotipy.Spotify):
             )
         )
 
-    def create_discover_weekly_playlist(self):
+    def create_discover_weekly_playlist(self) -> None:
         if self.discover_weekly_id:
             new_playlist = self.create_playlist(
                 self.playlist_name or f"{self.start_of_week} Re-Discover Weekly"
             )
             if self.playlist_name:
-                existing_tracks = self.get_track_ids_from_playlist(new_playlist["id"])
+                existing_tracks = self.get_track_ids_from_playlist(new_playlist.id)
                 new_tracks = [
                     track_id
                     for track_id in self.discover_weekly_tracks
                     if track_id not in existing_tracks
                 ]
                 if new_tracks:
-                    self.playlist_add_items(new_playlist["id"], new_tracks)
+                    self.playlist_add_items(new_playlist.id, new_tracks)
             else:
                 self.playlist_replace_items(
-                    new_playlist["id"],
+                    new_playlist.id,
                     [track_id for track_id in self.discover_weekly_tracks],
                 )
+        
 
-    def create_playlist(self, playlist_name):
+    def create_playlist(self, playlist_name: str) -> Playlist:
         try:
             return self.find_playlist(playlist_name)
         except PlaylistNotFoundError:
-            return self.user_playlist_create(
+            created_playlist = self.user_playlist_create(
                 self.current_user()["id"], playlist_name, public=False
             )
+            return Playlist(name=created_playlist["name"], id=created_playlist["id"])
 
     @cached_property
-    def discover_weekly_id(self):
+    def discover_weekly_id(self) -> str:
         discover_weekly_playlist = self.find_playlist("Discover Weekly")
-        return discover_weekly_playlist["id"]
+        return discover_weekly_playlist.id
 
     @property
-    def discover_weekly_tracks(self):
+    def discover_weekly_tracks(self) -> KeysView:
         return self.get_track_ids_from_playlist(self.discover_weekly_id)
 
-    def find_playlist(self, playlist_name):
+    def find_playlist(self, playlist_name: str) -> Playlist:
         offset = 0
         while True:
             playlists = self.current_user_playlists(offset=offset).get("items")
@@ -65,11 +76,11 @@ class Spotify(spotipy.Spotify):
                 raise PlaylistNotFoundError(f"playlist not found {playlist_name}")
             for playlist in playlists:
                 if playlist["name"] == playlist_name:
-                    return playlist
+                    return Playlist(name=playlist["name"], id=playlist["id"])
             offset += 50
 
-    def get_track_ids_from_playlist(self, playlist_id):
-        track_ids = {}
+    def get_track_ids_from_playlist(self, playlist_id: str) -> KeysView:
+        track_ids: Dict[str, None] = {}
         offset = 0
         while True:
             tracks = self.playlist_tracks(playlist_id, offset=offset)
@@ -83,7 +94,7 @@ class Spotify(spotipy.Spotify):
         return track_ids.keys()
 
     @property
-    def start_of_week(self):
+    def start_of_week(self) -> str:
         now = datetime.datetime.now()
         monday = now - datetime.timedelta(days=now.weekday())
         return monday.strftime("%B %d '%y")
@@ -98,3 +109,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     spotify = Spotify(args.playlist_name)
     spotify.create_discover_weekly_playlist()
+    print("success!")
